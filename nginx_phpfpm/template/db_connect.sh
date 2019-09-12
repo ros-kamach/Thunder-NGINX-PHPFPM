59d7acfe5415cfcd787ac7be8af2efb31d54e166
ros-kamach <47714061+ros-kamach@users.noreply.github.com>
2019-09-12 15:11:46 +0300

    Update db_connect.sh

--- nginx_phpfpm/template/db_connect.sh
+++ nginx_phpfpm/template/db_connect.sh
@@ -9,6 +9,7 @@ MYSQL_HOST=$6
 
 if [[ "${ENV}" == "yes" ]]
     then
+        mv /usr/share/nginx/settings.php /usr/share/nginx/html/sites/default/
         sed -i "s|;*memory_limit =.*|memory_limit = ${PHP_MEMORY_LIMIT}|i" /etc/php/7.2/php.ini
         sed -i "s|MYSQL_DATABASE|'database' => '${MYSQL_DATABASE}',|i" /usr/share/nginx/html/sites/default/settings.php
         sed -i "s|MYSQL_PASSWORD|'password' => '${MYSQL_PASSWORD}',|i" /usr/share/nginx/html/sites/default/settings.php
@@ -17,6 +18,7 @@ if [[ "${ENV}" == "yes" ]]
         tail -n 9  /usr/share/nginx/html/sites/default/settings.php
     else
         echo  Skip connection to Data_Base. Enviroment "$CONNECT_TO_DB" is  '' ${ENV}'', must be "yes"
+        rm -f ./db_connect.sh
 fi
 echo PHP_MEMORY_LIMIT=${PHP_MEMORY_LIMIT}
 echo MYSQL_DATABASE=${MYSQL_DATABASE}
