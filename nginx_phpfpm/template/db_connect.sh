eca8d370cd90e2140758777d858b569f8e3e7072
ros-kamach <47714061+ros-kamach@users.noreply.github.com>
2019-09-13 12:40:46 +0300

    Update db_connect.sh

--- nginx_phpfpm/template/db_connect.sh
+++ nginx_phpfpm/template/db_connect.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 # Add Enviroment to Progect "CONNECT_TO_DB=yes" to apply script
-ENV=$1
-PHP_MEMORY_LIMIT=$2
-MYSQL_USER=$3
-MYSQL_DATABASE=$4
-MYSQL_PASSWORD=$5
-MYSQL_PORT=$6
-MYSQL_HOST=$7
 
-if [[ "${ENV}" == "yes" ]]
+if [[ "${CONNECT_TO_DB}" == "yes" ]]
     then
         mv /usr/share/nginx/settings.php /usr/share/nginx/html/sites/default/
         sed -i "s|;*memory_limit =.*|memory_limit = ${PHP_MEMORY_LIMIT}|i" /etc/php/7.2/php.ini
@@ -19,7 +12,10 @@ if [[ "${ENV}" == "yes" ]]
         sed -i "s|MYSQL_HOST|'port' => '${MYSQL_HOST}',|i" /usr/share/nginx/html/sites/default/settings.php
         tail -n 9  /usr/share/nginx/html/sites/default/settings.php
     else
-        echo  Skip connection to Data_Base. Enviroment "$CONNECT_TO_DB" is  '' ${ENV}'', must be "yes"
+        echo "###"
+        echo  Skip connection to Data_Base. Enviroment "CONNECT_TO_DB" is  $CONNECT_TO_DB, must be "yes"
+        rm -f settings.php
+        echo "###"
 fi
 echo PHP_MEMORY_LIMIT=${PHP_MEMORY_LIMIT}
 echo MYSQL_DATABASE=${MYSQL_DATABASE}
