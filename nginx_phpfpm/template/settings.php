113dce22488577578328b37de11e3c2cd6aa47cc
ros-kamach <47714061+ros-kamach@users.noreply.github.com>
2019-09-18 12:42:33 +0300

    Update settings.php

--- nginx_phpfpm/template/settings.php
+++ nginx_phpfpm/template/settings.php
@@ -303,7 +303,7 @@ $settings['hash_salt'] = '';
  * After finishing the upgrade, be sure to open this file again and change the
  * TRUE back to a FALSE!
  */
-$settings['update_free_access'] = UPDATE_FREE_ACCESS;
+$settings['update_free_access'] = UPDATE_ACCESS;
 
 /**
  * External access proxy settings:
