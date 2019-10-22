d87154403b7dc144b5aac2bbf0456230dab024de
ros-kamach <47714061+ros-kamach@users.noreply.github.com>
2019-10-22 09:42:30 +0300

    Update drush.sh

--- nginx_phpfpm/template/drush.sh
+++ nginx_phpfpm/template/drush.sh
@@ -41,9 +41,9 @@ if [[ "${SITE_INSTALL}" == "yes" ]]
                 echo "HTML"
                 ls -la /usr/share/nginx/html
                 cd /usr/share/nginx/html/
-                vendor/bin/drush -y si standard \
+                vendor/bin/drush -y si \
                 --db-url=mysql://${MYSQL_USER}:${MYSQL_PASSWORD}@${MYSQL_HOST}/${MYSQL_DATABASE} \
-                --site-name=Thunder --account-name=${SITE_USER} --account-pass=${SITE_PASSWORD}
+                --site-name=Thunder --account-name=${SITE_USER} --account-pass=${SITE_PASSWORD} --sites-subdir=default
             else
                 echo "###"
                 echo  Skip Install . DataBase exist.
